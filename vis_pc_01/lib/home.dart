45c805daa7683c7b98a5c709271a3d850540a209
Andrés Cornejo <31871621+andrescornejo@users.noreply.github.com>
2020-04-02 17:03:41 -0600

    [improved] Main UI

--- vis_pc_01/lib/home.dart
+++ vis_pc_01/lib/home.dart
@@ -3,6 +3,8 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:vispc01/dataSeries.dart';
 
+import 'package:vispc01/bubble.dart';
+
 class HomePage extends StatelessWidget {
 
   Future<List<DataSeries>> getDataFromJSON(BuildContext context) async{
@@ -14,32 +16,54 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context){
     return Scaffold(
+        resizeToAvoidBottomInset: false, // set it to false
+
       appBar: AppBar(
-        title: Text("Data Parser"),
+        title: Text("vis-pc-01"),
       ),
-      body: Container(
-        child: FutureBuilder(
-          future: getDataFromJSON(context),
-          builder: (context,data){
-            if(data.hasData){
-              List<DataSeries> dataList = data.data;
-              return ListView.builder(
-                  itemCount: dataList.length,
-                  itemBuilder: (context, index){
-                    return ListTile(
-                      title: Text(dataList[index].age, style: TextStyle(
-                        fontSize: 25.0,
-                        fontWeight: FontWeight.bold
-                      ),),
-                      subtitle: Text(dataList[index].talkDisabled),
-                    );
-              });
-            } else {
-              return Center(child: CircularProgressIndicator(),);
-            }
-          }
-        )  
-      )
+      body: Column(children: <Widget>[
+        Container(
+          height: 539,
+          padding: EdgeInsets.all(10),
+          child: Card(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                children: <Widget>[
+                  Text("Visualización: Población con discapacidad en Costa Rica",
+                    style: Theme.of(context).textTheme.body2,
+                  ),
+                  Expanded(
+                      child: ShapesScatterPlotChart() //CHART GOES HERE
+                  )
+                ],
+              )
+            )
+          )
+        ),
+        Divider(),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            FlatButton(
+              onPressed: () {},
+              child: Icon(Icons.filter_list),
+              disabledColor: Colors.lightBlue[600],
+            ),
+            FlatButton(
+              onPressed: () {},
+              child: Icon(Icons.insert_chart),
+              disabledColor: Colors.lightBlue[600],
+            ),
+            FlatButton(
+              onPressed: () {},
+              child: Icon(Icons.info),
+              disabledColor: Colors.lightBlue[600],
+            ),
+          ],
+        )
+      ],)
     );
   }
 }
